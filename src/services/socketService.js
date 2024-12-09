7cef2702f0836c63e8ace2d108a21c5de287aca2
joan <joancallanaupalaurente@gmail.com>
2024-12-09 18:51:22 -0500

    9sd8f4sdf98

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -3,7 +3,7 @@ import { setPedidos } from "../redux/actions/pedidosActions";
 import { setPlatos } from "../redux/actions/platosActions";
 
 // Instancia del socket
-const socket = io("localhost:4000");
+const socket = io("https://socketserver-u5si.onrender.com/");
 
 const socketService = {
   connect: () => {
