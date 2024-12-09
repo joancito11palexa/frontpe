b466ee9e7e19ce9ea0e0bf637723595ef086b4a2
joan <joancallanaupalaurente@gmail.com>
2024-12-09 13:18:34 -0500

    dsfsdfd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Mesera } from "./pages/Mesera/Mesera";
 import { Cocina } from "./pages/Cocina";
 import { Ganancias } from "./pages/Ganancias/Ganancias";
@@ -7,18 +7,26 @@ import { NavBar } from "./pages/NavBar";
 import { Footer } from "./pages/Footer";
 import { Calculos } from "./pages/Calculos/Calculos";
 import { Ventas } from "./pages/Ventas/Ventas";
+import { Cliente } from "./pages/Cliente/Cliente";
+
 function App() {
+  const location = useLocation();
+
   return (
     <div>
-      <NavBar />
+      {/* Condicional para no renderizar el NavBar en /cliente */}
+      {location.pathname !== "/" && <NavBar />}
       <Routes>
         <Route path="/estadisticas" element={<Ganancias />} />
-        <Route path="/" element={<Mesera />} />
+        <Route path="/mesera" element={<Mesera />} />
         <Route path="/cocina" element={<Cocina />} />
         <Route path="/calculos" element={<Calculos />} />
         <Route path="/ventas" element={<Ventas />} />
+        <Route path="/" element={<Cliente />} />
       </Routes>
-      <Footer />
+      
+      {/* Condicional para no renderizar el Footer en /cliente */}
+      {location.pathname !== "/" && <Footer />}
     </div>
   );
 }
--- src/pages/Cliente/Cliente.jsx
+++ src/pages/Cliente/Cliente.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import socketService from "../../services/socketService";
+import Food1Icon from "../../assets/food1Icon.svg?react";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 export const Cliente = () => {
+  const dispatch = useDispatch();
+  const platos = useSelector((state) => state.platos);
+  const [fechaHoy, setFechaHoy] = useState("");
+
+  useEffect(() => {
+    // Obtener fecha actual en formato "Lunes 12 de noviembre"
+    const obtenerFechaHoy = () => {
+      const fecha = new Date();
+      setFechaHoy(format(fecha, "EEEE dd 'de' MMMM", { locale: es }));
+    };
+
+    obtenerFechaHoy();
+
+    // Conexión al servidor para sincronizar los platos
+    socketService.connect();
+    socketService.sincronizarPlatos(dispatch);
+
+    return () => {
+      socketService.limpiarPlatos();
+      socketService.disconnect();
+    };
+  }, [dispatch]);
+
   return (
-    <div>Cliente</div>
-  )
-}
+    <div className="clientePage">
+      <div className="encabezado">
+        <h3>Menú</h3>
+        <p className="fecha">{fechaHoy}</p> 
+      </div>
+
+      <Food1Icon className="iconFood1" />
+      <div className="platos-list">
+        {platos.length === 0 ? (
+          <div className="skeleton-loader">
+            <div className="skeleton-item"></div>
+            <div className="skeleton-item"></div>
+          </div>
+        ) : (
+          <div>
+            {/* Entradas */}
+            <h4>Entradas</h4>
+            <div className="platos-section">
+              {platos
+                .filter((plato) => plato.tipo === "entrada")
+                .map((plato) => (
+                  <div key={plato.id} className="plato-item">
+                    <div className="row justify-content-center align-items-center g-2">
+                      <div className="col-8">
+                        <p>{plato.nombre}</p>
+                      </div>
+                      <div className="col-4">
+                        <p>S/{plato.precio}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+
+            <h4>Principales</h4>
+            <div className="platos-section">
+              {platos
+                .filter((plato) => plato.tipo === "platoPrincipal")
+                .map((plato) => (
+                  <div key={plato.id} className="plato-item">
+                    <div className="row justify-content-center align-items-center g-2">
+                      <div className="col-8">
+                        <p>{plato.nombre}</p>
+                      </div>
+                      <div className="col-4">
+                        <p>S/{plato.precio}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
