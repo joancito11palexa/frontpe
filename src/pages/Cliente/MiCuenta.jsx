38f1674f8c16154c73a0577f2d93701135c83c1a
joan <joancallanaupalaurente@gmail.com>
2024-12-17 22:43:41 -0500

    e65e5e5ewrwer

--- src/pages/Cliente/MiCuenta.jsx
+++ src/pages/Cliente/MiCuenta.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import TupperIcon from "../../assets/iconsCuentaPage/iconTupper.svg?react";
 import MenuIcon from "../../assets/iconsCuentaPage/iconMenu.svg?react";
 import HistorialIcon from "../../assets/iconsCuentaPage/historialIcon.svg?react";
-import UserIcon from "../../assets/UserIcon1.svg?react";
+import UserIcon1 from "../../assets/UserIcon1.svg?react";
 import { Link } from "react-router-dom";
 export const MiCuenta = () => {
   const [userData, setUserData] = useState(null); // Estado para almacenar datos del usuario
@@ -67,7 +67,7 @@ export const MiCuenta = () => {
           </Link>
           <Link className="col-3">
             <div className="box">
-              <UserIcon />
+              <UserIcon1 />
             </div>
             <p>Mi cuenta</p>
           </Link>
