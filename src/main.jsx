81e0d496bd6ce620aadb7a420867604596c2719b
joan <joancallanaupalaurente@gmail.com>
2024-12-19 09:10:23 -0500

    df78s7s

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
-import "bootstrap/dist/js/bootstrap.bundle.js";
+
 import "./css/custom.min.css";
 import * as bootstrap from "bootstrap";
-import "./css/custom.min.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
